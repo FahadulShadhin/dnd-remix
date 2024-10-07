884c7509d158160a9002035e504b69e355bae64f
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-10-08 04:20:29 +0600

    make description expandable div smooth animation

--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface TaskCardProps {
   className?: string;
@@ -18,6 +18,12 @@ export function TaskCard({
   onDragStart,
 }: TaskCardProps) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) setContentHeight(contentRef.current.scrollHeight);
+  }, [description]);
 
   return (
     <div
@@ -28,27 +34,27 @@ export function TaskCard({
       <div className="flex justify-between items-center">
         <h2 className="text-lg font-bold text-primary-600">{title}</h2>
         <button
-          className="cursor-pointer focus:outline-none"
+          className="cursor-pointer focus:outline-none transition-transform duration-300 ease-in-out"
           onClick={() => setIsExpanded(!isExpanded)}
         >
-          {isExpanded ? (
-            <img
-              src="icons/chevron-up.svg"
-              alt="Drag icon"
-              width={15}
-              height={15}
-            />
-          ) : (
-            <img
-              src="icons/chevron-down.svg"
-              alt="Drag icon"
-              width={15}
-              height={15}
-            />
-          )}
+          <img
+            src={"icons/chevron-up.svg"}
+            alt="Toggle description"
+            width={15}
+            height={15}
+            className={`transform ${isExpanded ? "rotate-0" : "rotate-180"}`}
+          />
         </button>
       </div>
-      {isExpanded && <p className="text-md text-primary-500">{description}</p>}
+
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out`}
+        style={{ maxHeight: isExpanded ? `${contentHeight + 10}px` : "0" }}
+      >
+        <p ref={contentRef} className="text-md text-primary-500 mt-2">
+          {description}
+        </p>
+      </div>
     </div>
   );
 }
