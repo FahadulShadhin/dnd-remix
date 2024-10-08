f75a71c65a68d5260f40e42373edd44316466805
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-10-09 04:14:36 +0600

    add drag area component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
 import { tasks } from "~/data/tesks";
 import { TasksColumn } from "~/components/TasksColumn";
+import { useState } from "react";
 
 export default function Index() {
+  const [activeTask, setActiveTask] = useState<number | null>(null);
+
   return (
     <div className="bg-white">
       <div className="flex flex-col md:flex-row h-screen max-w-7xl mx-auto p-4">
@@ -9,18 +12,22 @@ export default function Index() {
           className="flex-1"
           title="To do"
           tasks={tasks.filter((task) => task.status === "to do")}
+          setActiveTask={setActiveTask}
         />
         <TasksColumn
           className="flex-1"
           title="In progress"
           tasks={tasks.filter((task) => task.status === "in progress")}
+          setActiveTask={setActiveTask}
         />
         <TasksColumn
           className="flex-1"
           title="Completed"
           tasks={tasks.filter((task) => task.status === "completed")}
+          setActiveTask={setActiveTask}
         />
       </div>
+      <h1>Active task: {activeTask}</h1>
     </div>
   );
 }
